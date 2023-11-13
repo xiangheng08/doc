671a5f01e11cbbd54a2af86b266519944489840a
laowan <11513313+laowans@user.noreply.gitee.com>
2023-11-13 11:29:14 +0800

    temp

--- docs/.vitepress/config/sidebar/git.ts
+++ docs/.vitepress/config/sidebar/git.ts
@@ -1,9 +1,15 @@
-import { define } from '../../utils'
-import { DefaultTheme } from 'vitepress'
+import { define } from '../../utils';
+import { DefaultTheme } from 'vitepress';
 
 export default define<DefaultTheme.SidebarItem[]>([
 	{
 		text: 'Git 命令大全',
 		link: '/git/commandList',
+	},
+	{
+		text: '错误',
+		link: '/git/error/index',
+		collapsed: false,
+		items: [],
 	},
 ]);
