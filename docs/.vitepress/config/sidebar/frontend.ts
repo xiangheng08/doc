b3da9dc7d05c8272d5daa010a1cae3938e108ad4
laowan <11513313+laowans@user.noreply.gitee.com>
2024-02-04 22:53:35 +0800

    增加码元和码点

--- docs/.vitepress/config/sidebar/frontend.ts
+++ docs/.vitepress/config/sidebar/frontend.ts
@@ -408,6 +408,10 @@ export default define<DefaultTheme.SidebarItem[]>([
         text: '利用延迟实现复杂动画',
         link: '/frontend/note/delayed-animation',
       },
+      {
+        text: '码元和码点',
+        link: '/frontend/note/code-point-and-code-unit'
+      }
     ],
   }
 ]);
