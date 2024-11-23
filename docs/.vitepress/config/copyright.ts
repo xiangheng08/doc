f403a13dc89b6f4601511130d85b07123de1364c
xiangheng <3074829365@qq.com>
2024-11-23 11:40:17 +0800

    update

--- docs/.vitepress/config/copyright.ts
+++ docs/.vitepress/config/copyright.ts
@@ -1,4 +1,4 @@
 // 著作权所属
-const _default = 'Copyright © 2022-present <a href="https://gitee.com/laowans" target="_blank" class="copyright-link">laowans</a>';
+const _default = 'Copyright © 2022-present <a href="https://gitee.com/xiangheng08" target="_blank" class="copyright-link">xiangheng</a>';
 
 export default process.env.COPYRIGHT || _default;
--- docs/frontend/electron/docsNav.md
+++ docs/frontend/electron/docsNav.md
@@ -1,5 +1,5 @@
 # 文档导航
 
 - [官网](https://www.electronjs.org/zh)
-- [electron-builder]([link](https://www.electron.build/))
+- [electron-builder](https://www.electron.build/)
 - [Electron 防止重复启动](https://www.electronjs.org/zh/docs/latest/api/app#apprequestsingleinstancelockadditionaldata)
