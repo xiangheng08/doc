5484a1f0a6f4b4e82782e76462d8755f782ac107
xiangheng <3074829365@qq.com>
2024-12-03 10:57:50 +0800

    init

---
+++ .vitepress/config.ts
@@ -0,0 +1,46 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'My doc',
+  description: 'A VitePress Site',
+  srcDir: 'src',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  },
+
+  markdown: {
+    theme: 'github-dark'
+  },
+
+  vite: {
+    server: {
+      port: 9527
+    },
+    resolve: {
+      alias: {
+        '@theme': fileURLToPath(new URL('./theme', import.meta.url))
+      }
+    }
+  }
+})
