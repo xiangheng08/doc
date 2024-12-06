ccde0c4939cd42118622615fb2e236cdd81ae57e
xiangheng <3074829365@qq.com>
2024-12-06 17:48:05 +0800

    update

--- src/vscode/components/extension-list.vue
+++ src/vscode/components/extension-list.vue
@@ -1,3 +1,24 @@
+<script setup lang="ts">
+import list from './extension-list.json'
+</script>
+
 <template>
-  <div></div>
+  <div class="extension-list">
+    <div class="extension-item" v-for="item in list" :key="item.url">
+
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.extension-list {
+  width: 100%;
+  height: 100px;
+  margin-top: 3em;
+  background-color: var(--vp-c-bg-soft);
+  padding: 1rem 1.25rem;
+}
+.extension-item {
+  padding: 12px 16px;
+}
+</style>
