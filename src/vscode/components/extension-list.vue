50a93879e6990048206f2b353b04c833e1cbc135
laowan <11513313+laowans@user.noreply.gitee.com>
2024-12-07 16:26:48 +0800

    优化vscode扩展列表及图标路径

--- src/vscode/components/extension-list.vue
+++ src/vscode/components/extension-list.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import { withBase } from 'vitepress';
 import list from './extension-list.json'
 </script>
 
 <template>
-  <div class="extension-list">
+  <div class="extension-list" data-not-zoom>
     <div class="extension-item" v-for="item in list" :key="item.url">
-
+      <img class="logo" :src="withBase(item.logo)" :alt="item.name" data-no-zoom />
+      <div class="info">
+        <div class="title">{{ item.name }}</div>
+        <div class="description">{{ item.desc }}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,12 +18,36 @@ import list from './extension-list.json'
 <style lang="scss" scoped>
 .extension-list {
   width: 100%;
-  height: 100px;
   margin-top: 3em;
   background-color: var(--vp-c-bg-soft);
   padding: 1rem 1.25rem;
 }
 .extension-item {
   padding: 12px 16px;
+  display: flex;
+  &:not(:first-child){
+    margin-top: 0.5rem;
+  }
+  &:not(:last-child){
+    border-bottom: 1px solid var(--vp-c-divider);
+  }
+  .logo{
+    width: 56px;
+    height: 56px;
+    margin-right: 1rem;
+  }
+  .info {
+    flex: 1;
+    margin-bottom: 1rem;
+    .title {
+      font-size: 1.2rem;
+      font-weight: 600;
+      margin-bottom: 0.5rem;
+    }
+    .description {
+      font-size: 0.9rem;
+      color: var(--vp-c-text-2)
+    }
+  }
 }
 </style>
--- src/vscode/extension-list.md
+++ src/vscode/extension-list.md
@@ -1,7 +1,7 @@
 # vscode 扩展
 
 <script lang="ts" setup>
-  import ExtensionList from './components/extension-list.vue'
+import ExtensionList from './components/extension-list.vue'
 </script>
 
 <ExtensionList />
