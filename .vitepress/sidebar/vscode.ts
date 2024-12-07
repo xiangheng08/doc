68b17a5ca324a7b886f6b70b75c1551f2a2471c2
laowan <11513313+laowans@user.noreply.gitee.com>
2024-12-07 16:47:06 +0800

    add doc

--- .vitepress/sidebar/vscode.ts
+++ .vitepress/sidebar/vscode.ts
@@ -1,5 +1,7 @@
 import { DefaultTheme } from 'vitepress'
 
 export default [
+  { link: '/vscode/tips' },
+  { link: '/vscode/shortcut' },
   { link: '/vscode/extension-list' },
 ] satisfies DefaultTheme.Sidebar
