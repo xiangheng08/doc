df0a271827b9b344b0979daf358acba62c0b2d84
xiangheng <3074829365@qq.com>
2024-12-11 11:39:17 +0800

    优化图片预览功能

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
-import { useData, useRoute } from 'vitepress'
+import { useData, useRoute, useRouter } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { nextTick, provide, onMounted, watch } from 'vue'
-import mediumZoom from "medium-zoom"
+import mediumZoom from 'medium-zoom'
 import ScrollToTop from './components/scroll-to-top.vue'
 
 const { isDark } = useData()
@@ -11,51 +11,79 @@ const enableTransitions = () =>
   'startViewTransition' in document &&
   window.matchMedia('(prefers-reduced-motion: no-preference)').matches
 
-provide('toggle-appearance', async ({ clientX: x, clientY: y }: MouseEvent) => {
-  if (!enableTransitions()) {
-    isDark.value = !isDark.value
-    return
-  }
-
-  const clipPath = [
-    `circle(0px at ${x}px ${y}px)`,
-    `circle(${Math.hypot(
-      Math.max(x, innerWidth - x),
-      Math.max(y, innerHeight - y)
-    )}px at ${x}px ${y}px)`
-  ]
-
-  await document.startViewTransition(async () => {
-    isDark.value = !isDark.value
-    await nextTick()
-  }).ready
-
-  document.documentElement.animate(
-    { clipPath: isDark.value ? clipPath.reverse() : clipPath },
-    {
-      duration: 300,
-      easing: 'ease-in',
-      pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`
+provide(
+  'toggle-appearance',
+  async ({ clientX: x, clientY: y }: MouseEvent) => {
+    if (!enableTransitions()) {
+      isDark.value = !isDark.value
+      return
     }
-  )
-})
 
-const setupMediumZoom = ()=>{
-  nextTick(()=>{
+    const clipPath = [
+      `circle(0px at ${x}px ${y}px)`,
+      `circle(${Math.hypot(
+        Math.max(x, innerWidth - x),
+        Math.max(y, innerHeight - y),
+      )}px at ${x}px ${y}px)`,
+    ]
+
+    await document.startViewTransition(async () => {
+      isDark.value = !isDark.value
+      await nextTick()
+    }).ready
+
+    document.documentElement.animate(
+      { clipPath: isDark.value ? clipPath.reverse() : clipPath },
+      {
+        duration: 300,
+        easing: 'ease-in',
+        pseudoElement: `::view-transition-${
+          isDark.value ? 'old' : 'new'
+        }(root)`,
+      },
+    )
+  },
+)
+
+// 判断祖先元素是否有 data-no-zoom 属性
+const hasNoZoom = (element: HTMLElement) => {
+  while (element.parentElement) {
+    if ('noZoom' in element.dataset) {
+      return true
+    }
+    element = element.parentElement
+  }
+  return false
+}
+
+const setupMediumZoom = () => {
+  setTimeout(() => {
+    const images = Array.from(
+      document.querySelectorAll<HTMLElement>(
+        '.main img:not([data-no-zoom])',
+      ),
+    )
+
+    for (let i = images.length - 1; i >= 0; i--) {
+      if (hasNoZoom(images[i])) {
+        images.splice(i, 1)
+      }
+    }
+
     // 图片预览
-    mediumZoom(".main img:not([data-no-zoom])", { background: "rgba(0, 0, 0, 0.7)" })
-  })
+    mediumZoom(images, { background: 'rgba(0, 0, 0, 0.7)' })
+  }, 100)
 }
 
 onMounted(setupMediumZoom)
 
-const route = useRoute()
+const router = useRouter()
 
-watch(route, setupMediumZoom)
+router.onAfterRouteChanged = setupMediumZoom
 </script>
 
 <template>
-  <DefaultTheme.Layout >
+  <DefaultTheme.Layout>
     <template #layout-bottom>
       <ScrollToTop />
     </template>
