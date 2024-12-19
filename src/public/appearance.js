b3a513b0bca55dbdc80922afadad943edafa6b67
xiangheng <3074829365@qq.com>
2024-12-19 16:17:06 +0800

    添加系统主题检测以设置页面颜色模式

--- src/public/appearance.js
+++ src/public/appearance.js
@@ -4,6 +4,13 @@
   if (url.searchParams.has('appearance')) {
     document.documentElement.style.colorScheme =
       url.searchParams.get('appearance')
+  } else {
+    // 根据系统主题设置
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.documentElement.style.colorScheme = 'dark'
+    } else {
+      document.documentElement.style.colorScheme = 'light'
+    }
   }
 
   window.addEventListener('message', (event) => {
