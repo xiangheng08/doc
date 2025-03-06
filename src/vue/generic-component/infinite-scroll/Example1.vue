6029c8b804e94f50d9125ca7797b92739b145226
xiangheng <3074829365@qq.com>
2025-03-06 19:57:24 +0800

    feat: 移除Example1.vue中初始化加载loadMore调用

--- src/vue/generic-component/infinite-scroll/Example1.vue
+++ src/vue/generic-component/infinite-scroll/Example1.vue
@@ -70,7 +70,4 @@ const fetchData = async (page: number) => {
 const random = (max: number, min = 0) => {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
-
-// 初始化加载
-loadMore()
 </script>
