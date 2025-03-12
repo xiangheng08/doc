204773443c0a02a8333081f47c5b730a22b42cdc
xiangheng <3074829365@qq.com>
2025-03-12 16:32:27 +0800

    refactor(.vitepress): 重构 demo-dynamic-import 插件
    - 优化代码结构，提高可读性和可维护性
    - 添加导入语句时的注释，便于理解
    - 改进对 __returned__ 和 _sfc_main 的处理逻辑
    - 调整正则表达式，提高匹配准确性

--- .vitepress/plugins/demo-dynamic-import.ts
+++ .vitepress/plugins/demo-dynamic-import.ts
@@ -1,65 +1,5 @@
 import type { Plugin } from 'vitepress'
 
-// 匹配占位符
-const placeholderRegex = /@ep_([^\(]+)\(([^\)]+)\)/g
-// 匹配正则关键字
-const keywordRegex = /[.*+?^${}()|[\]\\]/g
-// 转义正则关键字
-const escapeRegex = (str: string) => str.replace(keywordRegex, '\\$&')
-// 创建 _createTextVNode("%ep_component|path%") 匹配正则
-const createTextVNodeRegex = (placeholder: string) => {
-  return new RegExp(
-    `_createTextVNode\\("${escapeRegex(placeholder)}"\\)`,
-    'g',
-  )
-}
-
-const setupCreateVNodeTemplate = '_createVNode($setup["%"])'
-
-// 匹配 const __returned__ = { ... } 结构
-const returnedRegex = /const __returned__ = \{([^}]+)\}/
-
-const addReturned = (code: string, component: string): string => {
-  const match = code.match(returnedRegex)
-
-  if (match) {
-    // 获取匹配到的内容，并在其中添加新的组件
-    const existingComponents = match[1].trim()
-    const newComponents = existingComponents
-      ? `${existingComponents}, ${component}`
-      : component
-
-    // 替换原代码中的 __returned__ 部分
-    const updatedCode = code.replace(
-      returnedRegex,
-      `const __returned__ = { ${newComponents} }`,
-    )
-    return updatedCode
-  } else {
-    // 如果没有匹配到，可能是没有使用 setup 尝试转化成 setup 组件
-    // 匹配 const _sfc_main = {name:"test/2.md"}
-    const sfcMainRegex = /const _sfc_main = (\{[^\}]+\})/
-    const [, sfcMain] = code.match(sfcMainRegex) || []
-    if (sfcMain) {
-      const insert = `const __default__ = ${sfcMain}
-const _sfc_main = /*@__PURE__*/Object.assign(__default__, {
-  setup(__props, { expose: __expose }) {
-  __expose();
-
-
-const __returned__ = { __pageData, ${component} }
-Object.defineProperty(__returned__, '__isScriptSetup', { enumerable: false, value: true })
-return __returned__
-}
-
-})`
-      code = code.replace(sfcMainRegex, insert)
-    }
-  }
-
-  return code
-}
-
 const demoDynamicImport = (): Plugin => {
   return {
     name: 'vite-plugin-dynamic-import',
@@ -96,11 +36,9 @@ const demoDynamicImport = (): Plugin => {
       if (imports.length === 0) return
 
       // 生成导入语句
-      const importStatements =
-        '// demo container imports\n' +
-        imports
-          .map((v) => `import ${v.componentName} from "${v.path}"`)
-          .join('\n')
+      const importStatements = imports
+        .map((v) => `import ${v.componentName} from "${v.path}"`)
+        .join('\n')
 
       // 将导入语句注入到 Vue SFC
       newCode = importStatements + '\n' + newCode
@@ -108,6 +46,67 @@ const demoDynamicImport = (): Plugin => {
       return newCode
     },
   }
+}
+
+// 匹配占位符
+const placeholderRegex = /@ep_([^\(]+)\(([^\)]+)\)/g
+// 匹配正则关键字
+const keywordRegex = /[.*+?^${}()|[\]\\]/g
+// 转义正则关键字
+const escapeRegex = (str: string) => str.replace(keywordRegex, '\\$&')
+// 创建 _createTextVNode("%ep_component|path%") 匹配正则
+const createTextVNodeRegex = (placeholder: string) => {
+  return new RegExp(
+    `_createTextVNode\\("${escapeRegex(placeholder)}"\\)`,
+    'g',
+  )
+}
+
+const setupCreateVNodeTemplate = '_createVNode($setup["%"])'
+
+// 匹配 const __returned__ = {...} 结构
+const returnedRegex = /const __returned__ = \{([^}]+)\}/
+
+// 匹配 const _sfc_main = {...}
+const sfcMainRegex = /const _sfc_main = (\{[^\}]+\})/
+
+const addReturned = (code: string, component: string): string => {
+  const match = code.match(returnedRegex)
+
+  if (match) {
+    // 获取匹配到的内容，并在其中添加新的组件
+    const existingComponents = match[1].trim()
+    const newComponents = existingComponents
+      ? `${existingComponents}, ${component}`
+      : component
+
+    // 替换原代码中的 __returned__ 部分
+    const updatedCode = code.replace(
+      returnedRegex,
+      `const __returned__ = { ${newComponents} }`,
+    )
+    return updatedCode
+  } else {
+    // 如果没有匹配到，可能是没有使用 setup 尝试转化成 setup 组件
+    const [, sfcMain] = code.match(sfcMainRegex) || []
+    if (sfcMain) {
+      const insert = `const __default__ = ${sfcMain}
+const _sfc_main = /*@__PURE__*/Object.assign(__default__, {
+  setup(__props, { expose: __expose }) {
+  __expose();
+
+
+const __returned__ = { __pageData, ${component} }
+Object.defineProperty(__returned__, '__isScriptSetup', { enumerable: false, value: true })
+return __returned__
+}
+
+})`
+      code = code.replace(sfcMainRegex, insert)
+    }
+  }
+
+  return code
 }
 
 export default demoDynamicImport
