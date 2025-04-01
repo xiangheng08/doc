8357a16cbd0eae46d19a5f344d0956f841f2b66d
xiangheng <3074829365@qq.com>
2025-04-01 09:42:02 +0800

    docs(wx-mp): 添加禁止滚动功能说明
    - 在微信小程序中，弹窗弹出后禁止页面滚动
    - 在弹窗的根元素上加上 catchtouchmove="true" 属性

--- .vitepress/sidebar/wx-mp.ts
+++ .vitepress/sidebar/wx-mp.ts
@@ -2,4 +2,5 @@ import { DefaultTheme } from 'vitepress'
 
 export default [
   { link: '/wx-mp/' },
+  { link: '/wx-mp/not-scroll' },
 ] satisfies DefaultTheme.Sidebar
---
+++ src/wx-mp/not-scroll.md
@@ -0,0 +1,7 @@
+# 禁止滚动
+
+弹窗弹出后禁止页面滚动，在弹窗的根元素上加上 `catchtouchmove="true"`
+
+```html
+<view class="dialog" catchtouchmove="true">...</view>
+```
