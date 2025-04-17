5b34c1120ed6ee46309fc1bf95f82e66a81cc0a1
xiangheng <3074829365@qq.com>
2025-04-17 11:21:56 +0800

    docs(web-api): 添加 WebSocket 相关文档
    - 新增 WebSocket 概述、握手过程、客户端 API 使用示例
    - 介绍 WebSocket 服务器实现和常用库
    - 添加参考链接和学习资源

--- .vitepress/sidebar/web-api.ts
+++ .vitepress/sidebar/web-api.ts
@@ -4,4 +4,5 @@ export default [
   { link: '/web-api/clipboard' },
   { link: '/web-api/formdata' },
   { link: '/web-api/url' },
+  { link: '/web-api/websocket' },
 ] satisfies DefaultTheme.Sidebar
