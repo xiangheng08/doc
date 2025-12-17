97401008c88bd6a0b8969e51132ff5e49d4654d1
xiangheng <11513313+laowans@user.noreply.gitee.com>
2025-12-17 21:36:13 +0800

    docs(http): 添加 HTTP 版本演进文档
    新增 HTTP 版本演进的详细说明文档，涵盖从 HTTP/0.9 到 HTTP/3 的各个版本特性、
    改进点以及使用现状。同时在侧边栏中添加该文档的导航链接，方便用户查阅协议发展历史。

--- .vitepress/sidebar/http.ts
+++ .vitepress/sidebar/http.ts
@@ -4,5 +4,6 @@ export default [
   { link: '/http/index' },
   { link: '/http/https' },
   { link: '/http/status' },
+  { link: '/http/versions' },
   { link: '/http/restful-api' },
 ] satisfies DefaultTheme.Sidebar
