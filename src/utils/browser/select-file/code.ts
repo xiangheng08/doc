02bc8e87dcb266fda8e42b186cd45ed25b7c6250
xiangheng <xiangheng08@qq.com>
2026-04-24 14:53:04 +0800

    feat(utils): 优化selectFile函数实现并完善文档注释

--- src/utils/browser/select-file/code.ts
+++ src/utils/browser/select-file/code.ts
@@ -1,6 +1,8 @@
 export interface SelectFileOptions {
   /**
    * 文件类型
+   *
+   * @see [MDN Reference](https://developer.mozilla.org/zh-CN/docs/Web/HTML/Reference/Attributes/accept)
    */
   accept?: string
   /**
@@ -12,22 +14,25 @@ export interface SelectFileOptions {
 /**
  * 选择文件
  * @example
+ * ```js
  * selectFile({ accept: 'image/*', multiple: true }).then(files => {
  *     console.log(files)
  * })
+ * ```
  */
-export function selectFile(options?: SelectFileOptions) : Promise<File[]> {
+export function selectFile(options?: SelectFileOptions): Promise<File[]> {
   return new Promise<File[]>((resolve, reject) => {
     const { accept, multiple } = options || {}
     const input = document.createElement('input')
     input.type = 'file'
     if (accept) input.accept = accept
     if (multiple) input.multiple = multiple
-    input.click()
     input.onchange = () => {
       const files: File[] = []
-      for (let i = 0; i < input.files!.length; i++) {
-        files.push(input.files![i])
+      if (input.files) {
+        for (const file of input.files) {
+          files.push(file)
+        }
       }
       if (files.length > 0) {
         resolve(files)
@@ -41,5 +46,6 @@ export function selectFile(options?: SelectFileOptions) : Promise<File[]> {
     input.oncancel = () => {
       reject(new Error('CANCEL'))
     }
+    input.click()
   })
 }
