3f33d706322132af94eedba00643f9ecf8ef9f38
xiangheng <xiangheng08@qq.com>
2026-07-03 10:06:06 +0800

    feat(utils): 更新 isEmpty 函数以支持空 Map 和空 Set 的判断

--- src/utils/typeof/code/complex.ts
+++ src/utils/typeof/code/complex.ts
@@ -1,14 +1,16 @@
-import { isObject, isArray } from './base'
+import { isObject, isArray, isMap, isSet } from './base'
 
 /**
  * 判断是否为空
  *
- * 可以判断空字符串、false、null、undefined、NaN、空对象、空数组
+ * 可以判断空字符串、false、null、undefined、NaN、空对象、空数组、空 Map、空 Set
  */
 export function isEmpty(value: unknown) {
   return (
     (!value && value !== 0) ||
-    (isObject(value) && Object.keys(value).length === 0) ||
-    (isArray(value) && value.length === 0)
+    (isArray(value) && value.length === 0) ||
+    (isMap(value) && value.size === 0) ||
+    (isSet(value) && value.size === 0) ||
+    (isObject(value) && Object.keys(value).length === 0)
   )
 }
